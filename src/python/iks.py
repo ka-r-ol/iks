a433f218a05f2b0ba6a1ca280b89c3e5f53f2e6c
ka-r-ol <karol.lemanski@wp.pl>
2021-10-26 18:34:07 +0200

    refactor global_ctx + add edit

--- src/python/iks.py
+++ src/python/iks.py
@@ -1,22 +1,10 @@
-import sys
-import os
 import logging
-from simple_menu import MainMenu
+from lib import MainMenu, global_ctx
 
 if __name__ == "__main__":
     try:
-        #menu_path = sys.argv[1]
-        menu_path = os.environ['IKS_MENU_DIR']
-        if not menu_path:
-            print("no IKS_MENU_DIR set")
-            exit(0)
-        quick_execution = []
-        if len(sys.argv) > 1:
-            quick_execution = sys.argv[1:]
-        menu = MainMenu(menu_path=menu_path)
-        menu.run(quick_execution=quick_execution)
+        MainMenu().run(quick_execution=global_ctx.quick_execution)
     except KeyboardInterrupt:
         print("\nBye bye")
-        exit(0)
     except Exception as err:
         logging.error(f"Somethings gone wrong: {str(err)}", exc_info=True)
