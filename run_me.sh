3ca2e6c6412eeda5cd320e55a6ea7790f80716e2
ka-r-ol <karol.lemanski@wp.pl>
2023-11-11 11:28:55 +0100

    migrate to poetry

--- run_me.sh
+++ run_me.sh
@@ -1,6 +1,7 @@
 #!/usr/local/bin/bash
 
 cd $IKS_BASE_DIR
-source venv/bin/activate
+# source .venv/bin/activate
 #python src/python/iks.py $IKS_MENU_DIR $@
-python src/python/iks.py $@
+#poetry run python3 src/python/iks.py $@
+python3 src/python/iks.py $@
